674c4b08c392d4b26bfcb142d8b76d9dc7d43061
Vishal Thakur <vishalthakur.vt171@gmail.com>
2021-06-18 17:31:19 +0530

    make it better

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports ={
+    MONGOURI:"mongodb+srv://Pyther:1lu8udk1@insta.jgdj3.mongodb.net/test",
+    JWT_SECRET:"dfbmgbrujsxcbn",
+    SENDGRID_API:"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"
+}
+
+
+// if(process.env.NODE_ENV==='production'){
+//     module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+// }
